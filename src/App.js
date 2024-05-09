0559f52a198d8438196420c9e85e3ec85b39d439
twinkangaroos <132538451+twinkangaroos@users.noreply.github.com>
2024-05-09 17:42:02 +0900

    Add FontAwesome, Add DocumentsList.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Edy from './Edy'
 import EntryConstruction from './EntryConstruction'
+import DocumentsList from './DocumentsList'
 
 function App() {
   return (
     <div className="App">
-      <EntryConstruction />
+      <DocumentsList />
     </div>
   );
 }
